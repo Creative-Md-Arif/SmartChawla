199461962ab14eaa47e41efb248cfa2dd1d0f66c
Md.Arif <157142088+Creative-Md-Arif@users.noreply.github.com>
2026-04-01 15:52:34 +0600

    Update userModel.js

--- smart-chawla-backend/src/models/userModel.js
+++ smart-chawla-backend/src/models/userModel.js
@@ -145,14 +145,14 @@ const userSchema = new mongoose.Schema(
 );
 
 // Indexes - Removed userSchema.index({ email: 1 }) because 'unique: true' already creates it
-userSchema.index({ role: 1 });
+userSchema.index({ email: 1, isActive: 1, isVerified: 1 }); 
 
 // Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     return next();
   }
-  const salt = await bcrypt.genSalt(12);
+  const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
